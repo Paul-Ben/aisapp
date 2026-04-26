e102e258cb839266afd6e3b555d38c081c34c1e7
Paul-Ben Ajene <paulben.ajene@gmail.com>
2026-04-26 23:05:13 +0100

    Update footer and navbar links: replace static links with route helpers and comment out unused menu item

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -6,6 +6,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Alven International Schools - Building The Future')</title>
     
+    {{-- favicon --}}
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/images/logo.jpg')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/images/logo.jpg')}}">
+    
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     
